830f96d0777586d9820cd704265970760bad901b
marcovalla <42840969+marcovalla@users.noreply.github.com>
2018-11-09 23:57:28 -0300

    Ragnarok-desktop

---
+++ Ragnarok-desktop/src/ragnarok/paquete/Main.java
@@ -0,0 +1,20 @@
+ package ragnarok.paquete;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import ragnarok.principal.Juego;
+
+public class Main {
+	public static void main(String []args) {
+		
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Ragnarok";
+		cfg.width=800; //Ancho
+		cfg.height=600; //Alto
+		cfg.useGL30=false;
+		cfg.resizable=false; //Para que no modifique el tamaño de la pantalla
+		
+		new LwjglApplication(new Juego(),cfg);
+	}
+}
